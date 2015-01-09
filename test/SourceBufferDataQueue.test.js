ede4ab8fd71a9c054eb5f816eb14d98b51019638
heff <git@heff.me>
2015-01-08 16:16:59 -0800

    Added a testing setup with Karma and QUnit

---
+++ test/SourceBufferDataQueue.test.js
@@ -0,0 +1,5 @@
+var SourceBufferDataQueue = require('../src/js/sourceBuffer/SourceBufferDataQueue.js');
+
+QUnit.test("SourceBufferDataQueue exists", function(assert) {
+  assert.ok(SourceBufferDataQueue, "SourceBufferDataQueue does exist!");
+});
