f06b58479b94356ad901b1a75124c58a547950f1
Christian Pillsbury <cpillsbury@digitalprimates.net>
2015-01-27 13:17:11 -0600

    Added sinon to project and karma frameworks list.

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: [ 'browserify', 'qunit' ],
+    frameworks: [ 'browserify', 'qunit', 'sinon' ],
 
     browserify: {
       debug: true
